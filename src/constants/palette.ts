e35af224cac524484271a4229f6396e1a05eeb1f
shtjdqhd <shtjdqhd@naver.com>
2023-05-03 01:19:20 +0900

    style: palette 변경

--- src/constants/palette.ts
+++ src/constants/palette.ts
@@ -1,5 +1,10 @@
-export enum palette {
-  PRIMARY = 'red',
-  BACKGROUND = '#fff',
-  TEXT = '#000000',
-}
+export const palette = {
+  PRIMARY: '#A5B9FF',
+  BACKGROUND: '#eddfe0',
+  INNER_BACKGROUND: '#fff',
+  TEXT: '#343A40',
+  BUTTON_TEXT: '#fff',
+  COMPLETE: '#A5FFA8',
+  WAIT: '#a0a6b0',
+  DELTED: '#e34451',
+};
