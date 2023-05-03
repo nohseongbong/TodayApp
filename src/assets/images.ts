3d38b08ee90608d08a39b3f73b29b25d824267a7
shtjdqhd <shtjdqhd@naver.com>
2023-05-04 02:30:09 +0900

    style: image 추가

---
+++ src/assets/images.ts
@@ -0,0 +1,8 @@
+export const IMG = {
+  LOGO: require('./icon/logo.png'),
+  WRITE_ON: require('./icon/write_on.png'),
+  WRITE_OFF: require('./icon/write_off.png'),
+  LIST_ON: require('./icon/list_on.png'),
+  LIST_OFF: require('./icon/list_off.png'),
+  BACK: require('./icon/back.png'),
+};
