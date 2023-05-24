1102be1500ed6edcfea4a74c8969f38d451bc0f5
shtjdqhd <shtjdqhd@naver.com>
2023-05-24 11:17:32 +0900

    refactor: 버전 체크 함수 수정

--- src/lib/checkForUpdate.ts
+++ src/lib/checkForUpdate.ts
@@ -11,18 +11,24 @@ export const checkForUpdate = async () => {
     }
     if (updateAvailable.isNeeded) {
       const {currentVersion, latestVersion, storeUrl} = updateAvailable;
-      Alert.alert(
-        `최신 버전 업데이트`,
-        `${latestVersion} 버전 업데이트가 필요합니다.(현재 버전 ${currentVersion})`,
-        [
-          {
-            text: '업데이트',
-            onPress: () => {
-              Linking.openURL(storeUrl);
+
+      const currentMajorMinor = currentVersion.split('.').slice(0, 2).join('.');
+      const latestMajorMinor = latestVersion.split('.').slice(0, 2).join('.');
+
+      if (currentMajorMinor !== latestMajorMinor) {
+        Alert.alert(
+          '최신 버전 업데이트',
+          `${latestVersion} 버전 업데이트가 필요합니다. (현재 버전 ${currentVersion})`,
+          [
+            {
+              text: '업데이트',
+              onPress: () => {
+                Linking.openURL(storeUrl);
+              },
             },
-          },
-        ],
-      );
+          ],
+        );
+      }
     }
   } catch (error) {
     console.log(error);
