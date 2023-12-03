1a0f490e1ef93be51c7e7ec6aea81ab4722f53db
nohseongbong <shtjdqhd@naver.com>
2023-12-03 14:29:35 +0900

    feat: mobx 상태관리 filpper 체크 추가

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import {KeyboardAvoidingView, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigator from './src/navigation/RootNavigator';
+import {listStore} from './src/store/listStore';
+import {spy} from 'mobx';
+import {createMobxDebugger} from 'mobx-flipper';
 
 const App = () => {
+  spy(createMobxDebugger(listStore));
   return (
     <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <NavigationContainer>
