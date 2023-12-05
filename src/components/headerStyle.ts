232b2786c45859ae08f352565a7dec196921c26c
nohseongbong <shtjdqhd@naver.com>
2023-12-05 19:29:12 +0900

    style: 불필요한 코드 제거

--- src/components/headerStyle.ts
+++ src/components/headerStyle.ts
@@ -1,4 +1,4 @@
-import {Platform, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {ht, wt} from '../lib/responsiveSize';
 import {palette} from '../constants/palette';
 
