5a0560884d8b729fe6c99c890411165f0faedf29
nohseongbong <shtjdqhd@naver.com>
2024-01-02 00:20:44 +0900

    refactor: text 수정

--- src/data/testData.ts
+++ src/data/testData.ts
@@ -5,7 +5,7 @@ export const dummyData = function () {
   for (let index = 0; index < 40; index++) {
     arr.push({
       id: index,
-      title: '제목이다',
+      title: '',
       text: '',
       state: random(0, 100) % 2 === 0 ? '진행중' : '완료',
     });
